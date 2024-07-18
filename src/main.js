32b8a3abbe1db46d4caaa062289618f5773ca226
DestinyFrog <107517847+DestinyFrog@users.noreply.github.com>
2024-07-17 22:50:58 -0300

    update 'main' only use imports

--- src/main.js
+++ src/main.js
@@ -1,9 +1,7 @@
 import Header from './features/Header'
 import WindowError from './features/WindowError'
 import Atomo from './models/Atomo'
-import Molecula from './models/Molecula'
 import './style.css'
-import WindowMolecula3D from './windows/WindowMolecula3D'
 
 ( async () => {
 
